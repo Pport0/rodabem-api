2f0b0eb7e3011c2e44abc3846e53cc076a696654
pporto <pporto@dugregoriosistemas.com.br>
2026-03-04 18:26:40 -0300

    Manter Usuario + Autenticação de Usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -29,4 +29,94 @@ export class UsersService {
       },
     });
   }
+
+  async findAll(
+  page: number,
+  limit: number,
+  nome?: string,
+  status?: string,
+) {
+  const where: any = {};
+
+  if (nome) {
+    where.nome = {
+      contains: nome,
+      mode: 'insensitive',
+    };
+  }
+
+  if (status !== undefined) {
+    where.status = status === 'true';
+  }
+
+  const users = await this.prisma.user.findMany({
+    where,
+    skip: (page - 1) * limit,
+    take: limit,
+    orderBy: {
+      nome: 'asc',
+    },
+  });
+
+  return users.map(({ senhaHash, cpfHash, cpfEncrypted, ...user }) => user);
+}
+
+
+async update(id: number, data: CreateUserDto) {
+
+  const user = await this.prisma.user.findUnique({
+    where: { id },
+  });
+
+  if (!user) {
+    throw new BadRequestException('Usuário não encontrado');
+  }
+
+  const senhaHash = await bcrypt.hash(data.senha, 10);
+  const cpfEncrypted = encrypt(data.cpf);
+  const cpfHash = await bcrypt.hash(data.cpf, 10);
+
+  const updated = await this.prisma.user.update({
+    where: { id },
+    data: {
+      nome: data.nome,
+      email: data.email,
+      senhaHash,
+      cpfEncrypted,
+      cpfHash,
+      telefone: data.telefone,
+    },
+  });
+
+  const { senhaHash: s, cpfHash: c, cpfEncrypted: e, ...safe } = updated;
+  return safe;
+}
+
+async remove(id: number) {
+
+  const user = await this.prisma.user.findUnique({
+    where: { id },
+  });
+
+  if (!user) {
+    throw new BadRequestException('Usuário não encontrado');
+  }
+
+  // Simulação de vínculo ativo
+  const possuiVinculo = false;
+
+  if (possuiVinculo) {
+    throw new BadRequestException(
+      'Usuário possui vínculos ativos',
+    );
+  }
+
+  await this.prisma.user.delete({
+    where: { id },
+  });
+
+  return {
+    message: 'Usuário excluído com sucesso!',
+  };
+}
 }
